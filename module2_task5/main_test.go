959c38ccee6cd3bf562d538924b46299b95fa212
Giogap <giogap@gmail.com>
2023-01-25 17:28:19 -0500

    upload 5

--- module2_task5/main_test.go
+++ module2_task5/main_test.go
@@ -38,9 +38,9 @@ func Test_HelloHandler(t *testing.T) {
 		},
 		{
 			name:         "With two name parameters",
-			queryString:  "name=Alejo&name=Luiyi",
+			queryString:  "name=Giovanni&name=Silvana",
 			responseCode: 200,
-			body:         "Hello Luiyi!",
+			body:         "Hello Silvana!",
 		},
 	}
 	for _, tt := range tests {
